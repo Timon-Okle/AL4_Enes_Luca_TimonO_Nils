4e82be39e6f482310b60a082243a6b316fe812a2
NilsStadelmann <132067483+NilsStadelmann@users.noreply.github.com>
2023-06-16 00:58:45 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -13,53 +13,51 @@ const favoritedrinkField = document.getElementById("favoritedrink");
 
 
 
-
 // (2) Interaktionen festlegen
 firstnameField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 lastnameField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 
 emailField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 phoneField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 
 
 countryField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 
 favoritedrinkField.addEventListener("keyup", () => {
-  validateForm();
+    validateForm();
 });
 
 submitButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-  onClickSubmit();
+    event.preventDefault();
+    onClickSubmit();
 });
-
 
 
 
 
 const validateForm = () => {
-  
-  const email = emailField.value;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-  const phone = phoneField.value;
-  const firstName = firstnameField.value;
-  const lastName = lastnameField.value;
- 
-  if (email === "" || !emailRegex.test(email) || phone === "" ||firstName === "" ||lastName === "") {
-    submitButton.disabled = true;
-  } else {
-    submitButton.disabled = false;
-  }
+
+    const email = emailField.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phone = phoneField.value;
+    const firstName = firstnameField.value;
+    const lastName = lastnameField.value;
+
+    if (email === "" || !emailRegex.test(email) || phone === "" || firstName === "" || lastName === "") {
+        submitButton.disabled = true;
+    } else {
+        submitButton.disabled = false;
+    }
 }
 
 // (3) Interaktionen Code
@@ -67,39 +65,30 @@ const validateForm = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
 const onClickSubmit = async () => {
-  // Daten aus dem Formular für die Datenbank bereitstellen
-  const data = {
-    group: "al4", // SQL Gruppen Namen
-    pw: "8ec2f7ae", // SQL Passwort
-    tableName: "anmeldeformular", // Name der Tabelle in der SQL Datenbank
+    // Daten aus dem Formular für die Datenbank bereitstellen
+    const data = {
+        group: "al4", // SQL Gruppen Namen
+        pw: "8ec2f7ae", // SQL Passwort
+        tableName: "anmeldeformular", // Name der Tabelle in der SQL Datenbank
 
-    columns: {
-      // "email" Name der Spalte in der SQL Tabelle
-      // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
-      email: emailField.value,
-      firstname: firstnameField. value,
-      lastname: lastnameField. value,
-      phone: phoneField. value,
-      country: countryField.value,
-      favoritedrink: favoritedrinkField.value,
-      
-    },
-  };
-  // Speichert die Daten in der Datenbank
-  await databaseClient.insertInto(data);
+        columns: {
+            // "email" Name der Spalte in der SQL Tabelle
+            // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
+            email: emailField.value,
+            firstname: firstnameField.value,
+            lastname: lastnameField.value,
+            email: emailField.value,
+            phone: phoneField.value,
+            country: countryField.value,
+            favoritedrink: favoritedrinkField.value,
 
-  // Nach dem Speichern verschwindet das Formular, eine Dankeschön Nachricht erscheint
-  formContainer.classList.add("hidden");
-  thankYouContainer.classList.remove("hidden");
+        },
+    };
+    // Speichert die Daten in der Datenbank
+    await databaseClient.insertInto(data);
+
+    // Nach dem Speichern verschwindet das Formular, eine Dankeschön Nachricht erscheint
+    formContainer.classList.add("hidden");
+    thankYouContainer.classList.remove("hidden");
 };
